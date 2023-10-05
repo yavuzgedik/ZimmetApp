c5b63b9079a6a8a95d8e7ce2e6034a5ac0f5f76d
Yavuz Gedik <yavuzgedik@outlook.com>
2023-10-05 16:27:29 +0300

    last updated

--- ZimmetApp.WebUI/Controllers/MusteriController.cs
+++ ZimmetApp.WebUI/Controllers/MusteriController.cs
@@ -18,15 +18,27 @@ namespace ZimmetApp.WebUI.Controllers
                     .Include("ZimmetTanims")
                     //.Where(x => x.IsDeleted == false)
                     .Where(x => !x.IsDeleted)
+                    .OrderBy(x => x.MusteriKod)
                     .ToList();
 
                 return View(musteriler);
             }
         }
 
-        public ActionResult Ekle()
+        public ActionResult Ekle(Guid? musteri_id)
         {
-            return View();
+            if (musteri_id == null)
+            {
+                return View();
+            }
+            else
+            {
+                using (var db = new ZimmetDbContext())
+                {
+                    var musteri = db.Musteris.FirstOrDefault(x => x.Id == musteri_id);
+                    return View(musteri);
+                }
+            }
         }
 
         [HttpPost]
@@ -34,14 +46,52 @@ namespace ZimmetApp.WebUI.Controllers
         {
             using (var db = new ZimmetDbContext())
             {
-                musteri.MusteriAdi = musteri.MusteriAdi.ToUpper();
-                db.Musteris.Add(musteri);
-                db.SaveChanges();
+                var dbMusteri = db.Musteris.FirstOrDefault(x => x.Id == musteri.Id);
+                var control = db.Musteris.FirstOrDefault(x => x.MusteriKod == musteri.MusteriKod && !x.IsDeleted);
+
+                if (dbMusteri == null) // İLK KAYIT
+                {
+                    if (control == null)
+                    {
+                        musteri.MusteriAdi = musteri.MusteriAdi.ToUpper();
+                        db.Musteris.Add(musteri);
+                        db.SaveChanges();
+
+                        TempData["OK"] = "Müşteri Eklendi!";
+                        return RedirectToAction("Listele");
+                    }
+                    else
+                    {
+                        TempData["NO"] = "Müşteri Kodu Kullanıyor!";
+                        return View();
+                    }
+                }
+                else // GÜNCELLEME
+                {
+                    dbMusteri.MusteriAdi = musteri.MusteriAdi.ToUpper();
+
+                    if (control == null)
+                    {
+                        dbMusteri.MusteriKod = musteri.MusteriKod;
+
+                        TempData["OK"] = "Müşteri Güncellendi!";
+                    }
+                    else
+                    {
+                        if (control.MusteriKod != dbMusteri.MusteriKod)
+                        {
+                            TempData["NO"] = "Müşteri Kodu Kullanıyor! Müşteri Güncelledi";
+                        }
+                        else
+                        {
+                            TempData["OK"] = "Müşteri Güncellendi!";
+                        }
+                    }
+
+                    db.SaveChanges();
+                    return RedirectToAction("Listele");
+                }
             }
-
-            TempData["OK"] = "Müşteri Eklendi!";
-
-            return RedirectToAction("Listele");
         }
 
         public ActionResult Sil(Guid id)
@@ -49,7 +99,7 @@ namespace ZimmetApp.WebUI.Controllers
             using (var db = new ZimmetDbContext())
             {
                 var musteri = db.Musteris
-                    .FirstOrDefault(x=> x.Id == id);
+                    .FirstOrDefault(x => x.Id == id);
                 //db.Musteris.Remove(musteri); //SİL
 
                 musteri.IsDeleted = true;
